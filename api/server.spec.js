42ce76da87c400a2248d6054bceb055b6cc0861f
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-12 23:33:44 -0500

    Cleans up code of stray comments on server.spec

--- api/server.spec.js
+++ api/server.spec.js
@@ -20,30 +20,3 @@ describe('Server Sanity Check', () => {
         .expect('"Sanity Check"', done)
     })
   })
-
-
-
-
-
-
-
-
-// describe('server', function() {
-//     describe('GET /', function() {
-//         it('should return 200 OK', function() {
-//             return request(server)
-//             .get('/')
-//             .then(res => {
-//                 expect(res.status).toBe(200);
-//             });
-//         });
-
-//         it('should return JSON formatted response', function() {
-//             return request(server)
-//             .get('/')
-//             .then(res => {
-//                 expect(res.type).toMatch(/json/i);
-//             });
-//         });
-//     });
-// });
