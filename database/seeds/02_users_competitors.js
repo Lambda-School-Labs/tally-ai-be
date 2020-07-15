95308a0578b387212bab15a2751949ffdc397a41
Michael Phelps <michaeljphelps1978@gmail.com>
2020-07-14 23:12:05 -0500

    renames users_competition to users_competitors in seed file and table names within file

--- database/seeds/02_users_competitors.js
+++ database/seeds/02_users_competitors.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tallyweb.competitors').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('tallyweb.users_competitors').insert([
         {user_id: '1', business_id: '0-PyDCXxUXNZR6BI305evQ' },
         {user_id: '2', business_id: '0s0Xthk1JWrBUu74M81Xyg'},
         {user_id: '3', business_id: '0G-0qet57CmMA5qUm6gPFUTpg' },
